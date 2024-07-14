3b1575e3857e40a8e01b8ef666443443cd366285
JosemirFarias <josemirfbrito@gmail.com>
2024-07-14 14:13:18 -0300

    atualização pasta de exemplos

---
+++ ex_For/src/ex_ForArray.java
@@ -0,0 +1,11 @@
+public class ex_ForArray {
+ public static void main(String[] args) {
+    
+
+    String alunos[] = {"João", "Maria", "José", "Pedro", "Ana"};
+
+    for (String aluno : alunos){
+        System.out.println("Nome do aluno é: " + aluno);
+    }
+ }   
+}
---
+++ ex_For/src/ex_for.java
@@ -0,0 +1,15 @@
+// Código que exemplifica o uso do laço de repetição for
+public class ex_for {
+
+    // Método principal
+    public static void main(String[] args) {
+        
+        // Laço de repetição for
+        for (int carneirinhos = 1; carneirinhos <=20; carneirinhos ++ ){
+            
+            // Imprime a quantidade de carneirinhos
+            System.out.println("Carneirinho " + carneirinhos);
+        }
+        System.out.println("joãozinho dormiu");
+    }
+}
